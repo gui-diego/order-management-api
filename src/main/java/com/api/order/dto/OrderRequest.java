036ca1172bf15083f85fe5d4e3733a1aea0add1a
Gui <guidiegofreitas@yahoo.com.br>
2026-09-14 17:23:57 -0300

    Ajuste geral no retorno dos endpoints para adequar regras de negócio

--- src/main/java/com/api/order/dto/OrderRequest.java
+++ src/main/java/com/api/order/dto/OrderRequest.java
@@ -2,8 +2,10 @@ package com.api.order.dto;
 
 import com.api.orderItem.dto.OrderItemDTO;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record OrderRequest(@Valid List<OrderItemDTO> items) {
+public record OrderRequest(
+        List<@NotNull(message = "Os itens do pedido não podem ser nulos") @Valid OrderItemDTO> items) {
 }
